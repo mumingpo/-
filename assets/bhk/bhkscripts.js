04997494ddfcc9b817ef86bf5b19c090eec5a66a
Ming Mu <mumingpo@hotmail.com>
2018-03-19 19:13:03 -0400

    DRYed and modified navbar

--- assets/bhk/bhkscripts.js
+++ assets/bhk/bhkscripts.js
@@ -1,4 +1,4 @@
-var currentPosition=0;
+var currentSegment=0;
 var animationFinished=true;
 var numOfSegments=4;
 var endPosition;
@@ -7,87 +7,68 @@ $(document).ready(function(){
 //  $("p").click(function(){
 //    $(this).hide();
 //  });
+  //scrolling functions
   endPosition=$("#t"+numOfSegments.toString()).offset().top
-  $("html").on('mousewheel', function(e){
-    e.preventDefault();
-    if(e.originalEvent.wheelDelta > 0) {
-      //code for scrolling up
-      if(animationFinished){
-        if (currentPosition > 0){
-          currentPosition --;
-        }
-        else{
-          currentPosition=0;
-        }
-        animationFinished=false;
-        var positTop = $("#t"+currentPosition.toString()).offset().top;
-        $("#progressBar").animate({
-          "width": Math.floor(positTop/endPosition*100).toString()+"%"
-        })
-        $("html, body").animate({
-          scrollTop: positTop
-        }, 400, "swing", function(){animationFinished=true});
-      }
-      //console.log('scrolling up !');
-    }
-    else{
-      //code for scrolling down
-      if(animationFinished){
-        if(currentPosition<numOfSegments){
-          currentPosition++;
-        }
-        else {
-          currentPosition=numOfSegments;
-        }
-        animationFinished=false;
-        var positTop = $("#t"+currentPosition.toString()).offset().top;
-        $("#progressBar").animate({
-          "width": Math.floor(positTop/endPosition*100).toString()+"%"
-        })
-        $("html, body").animate({
-          scrollTop: positTop
-        }, 400, "swing", function(){animationFinished=true});
-      }
-      //console.log('scrolling down !');
-    }
-  });
-
-  $("#topbtn").on("click", function(){
-    currentPosition=0;
-    animationFinished=false;
+  function scrollToSegment(pos){
+    if (!animationFinished) return;
+    animationFinished = false;
+    currentSegment = pos;
+    var positTop = $("#t"+currentSegment.toString()).offset().top;
+    $("#progressBar").animate({
+      "width": Math.floor(positTop/endPosition*100).toString()+"%"
+    });
+    $("html, body").animate({
+      scrollTop: positTop
+    }, 400, "swing", function(){animationFinished=true});
+  }
+  function scrollToTop(){
+    if (!animationFinished) return;
+    animationFinished = false;
+    currentSegment = 0;
     $("#progressBar").animate({
       "width": "0%"
-    })
+    });
     $("html, body").animate({
       scrollTop: 0
-    }, 400, "swing", function(){animationFinished=true});
+    }, 400, "swing", function(){animationFinished=true});    
+  }
+  function scrollUp(){
+    if (!animationFinished) return;
+    if (currentSegment > 0){
+      scrollToSegment(currentSegment-1);
+    }
+    else {
+      scrollToTop();
+    }
+  }
+  function scrollDown(){
+    if (!animationFinished) return;
+    if (currentSegment == numOfSegments){
+      scrollToSegment(currentSegment);
+    }
+    else {
+      scrollToSegment(currentSegment+1);
+    }
+  }
+  
+  //binding the scroll functions
+  $("html").on("scroll", function(e){e.preventDefault();});
+  $("html").on('mousewheel', function(e){
+    e.preventDefault();
+    if(e.originalEvent.wheelDelta > 0){scrollUp();}
+    else{scrollDown();}
   });
-  $("#upbtn").on("click", function(){
-    if (currentPosition > 0){
-      currentPosition--;
-      animationFinished=false;
-      var positTop = $("#t"+currentPosition.toString()).offset().top;
-      $("#progressBar").animate({
-        "width": Math.floor(positTop/endPosition*100).toString()+"%"
-      })
-      $("html, body").animate({
-        scrollTop: positTop
-      }, 400, "swing", function(){animationFinished=true});
+  $("html").on("keydown", function(e){
+    if ([38, 33].indexOf(event.which) != -1){
+      scrollUp();
+    }
+    else if ([34, 40, 32].indexOf(event.which)!= -1) {
+      scrollDown();
     }
   });
-  $("#downbtn").on("click", function(){
-    if (currentPosition < numOfSegments){
-      currentPosition++;
-      animationFinished=false;
-      var positTop = $("#t"+currentPosition.toString()).offset().top;
-      $("#progressBar").animate({
-        "width": Math.floor(positTop/endPosition*100).toString()+"%"
-      })
-      $("html, body").animate({
-        scrollTop: positTop
-      }, 400, "swing", function(){animationFinished=true});
-    }
-  });
+  $("#topbtn").on("click", scrollToTop);
+  $("#upbtn").on("click", scrollUp);
+  $("#downbtn").on("click", scrollDown);
 
 
 });
